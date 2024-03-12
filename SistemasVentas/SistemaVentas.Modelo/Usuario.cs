773c6ada5f6b167549ddf830f372b0ca506067d3
LABORATORIO A <LABORATORIO A@LAB.EDU.BO>
2024-03-12 10:22:20 -0400

    VENTAS

---
+++ SistemasVentas/SistemaVentas.Modelo/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaVentas.Modelo
+{
+    internal class Usuario
+    {
+        public int IdUsuario { get; set; }
+
+        public int IdPersona { get; set; }
+
+        public string NombreUser { get; set; }
+
+        public string Contraseña { get; set; }
+
+        public DateTime FechaReg { get; set; }
+    }
+}
