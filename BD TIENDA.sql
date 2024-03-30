ac6b159ddda81d3aa315517b26e8a663e19aab2d
Juanco74443815 <juananel34@gmail.com>
2024-03-30 18:58:28 -0400

    Nuevos Cambios

--- BD TIENDA.sql
+++ BD TIENDA.sql
@@ -1,4 +1,3 @@
-use TIENDABD
 CREATE DATABASE TIENDABD
 
 
@@ -871,4 +870,8 @@ insert into detalleing values (19, 79, '2023-02-27 15:32:11', 71, 150, 250, 150,
 insert into detalleing values (20, 80, '2023-01-14 03:47:29', 91, 180, 200, 180, 'Exitoso');
 
 
+<<<<<<< HEAD
 select * from DETALLEING
+=======
+select * from DETALLEING
+>>>>>>> 1bcaab87270214d5e8cc333441c8083bd1c95a49
